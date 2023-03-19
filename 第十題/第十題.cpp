df9add5f2aa2d8ca044c5d8c55fbae3cc19a2c4c
USER <USER@DESKTOP-4L1KJ2I>
2023-03-19 21:41:54 +0800

    加入專案檔案。

---
+++ 第十題/第十題.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+int euc(int a, int b);
+int main()
+{
+	int in = 0, in2 = 0, c = 0;
+
+	cin >> in >> in2;
+	if (in < in2)
+	{
+		c = in;
+		in = in2;
+		in2 = c;
+	}
+	cout << euc(in, in2) << endl;
+	return 0;
+}
+int euc(int a, int b)
+{
+	int f = a % b;
+	if (f == 0) return b;
+	else return euc(b, f);
+}
